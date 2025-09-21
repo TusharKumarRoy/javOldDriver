4d8afbb159a8592f77da1e315b3a4cf9e022549d
Tushar Kumar Roy <tusharkumarroy.dev@gmail.com>
2025-09-21 13:39:22 +0600

    first

---
+++ student.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Student {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter & Setter
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+}
